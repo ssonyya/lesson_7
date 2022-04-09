16cf1f76e2f5318199591c191c3e107af5b3fb92
Софья Гордеева <sofyagordeeva2002@gmail.com>
2022-04-09 11:14:57 +0500

    ..

--- урок7/урок7.cpp
+++ урок7/урок7.cpp
@@ -6,8 +6,8 @@
 #include <GL/freeglut.h>
 #include "math_3d.h"
 
-GLuint VBO;
-GLuint gWorldLocation;
+GLuint VBO; //назначила GLuint в качестве глобальной переменной для хранения указателя на буфер вершин
+GLuint gWorldLocation; //указатель для доступа к всемирной матрице
 
 
 static const char* pVS = "                                                          \n\
@@ -32,9 +32,9 @@ void main()                                                                     
     FragColor = vec4(1.0, 0.0, 0.0, 1.0);                                           \n\
 }";
 
-static void RenderSceneCB()
+static void RenderSceneCB() //функцию отображения на экран
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT); //Очистка буфера кадра
 
     static float Scale = 0.0f;
 
@@ -48,15 +48,15 @@ static void RenderSceneCB()
     World.m[2][0] = 0.0f;        World.m[2][1] = 0.0f;         World.m[2][2] = 1.0f; World.m[2][3] = 0.0f;
     World.m[3][0] = 0.0f;        World.m[3][1] = 0.0f;         World.m[3][2] = 0.0f; World.m[3][3] = 1.0f;
 
-    glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World.m[0][0]);
+    glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World.m[0][0]); //загружаем матрицу в шейдер
 
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0); //Координаты вершин, используемые в буфере, рассматриваются как атрибут вершины с индексом 0 в фиксированной функции конвейера
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); //обратно привязываем наш буфер, приготавливая его для отрисовки
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); //Этот вызов говорит конвейеру как воспринимать данные внутри буфера
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLES, 0, 3); //мы рисуем треугольники вместо точек и принимаем 3 вершины вместо 1
 
-    glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(0); //Отключаем каждый атрибут вершины
 
     glutSwapBuffers();
 }
@@ -64,20 +64,25 @@ static void RenderSceneCB()
 
 static void InitializeGlutCallbacks()
 {
-    glutDisplayFunc(RenderSceneCB);
+    glutDisplayFunc(RenderSceneCB); //Функция обратного вызова, которая отрисовывает 1 кадр
     glutIdleFunc(RenderSceneCB);
 }
 
 static void CreateVertexBuffer()
 {
-    Vector3f Vertices[3];
+    //увеличили массив, что бы он мог содержать 3 вершины
+    Vector3f Vertices[3]; //создаем массив из 3 экземпляров структуры Vector3f (этот тип объявлен в math_3d.h) 
     Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
     Vertices[1] = Vector3f(1.0f, -1.0f, 0.0f);
     Vertices[2] = Vector3f(0.0f, 1.0f, 0.0f);
 
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    glGenBuffers(1, &VBO); //glGen* функция для генерации объектов переменных типов
+                           //Принимает 2 параметра: первый определяет количество объектов 
+                           //второй ссылка на массив типа GLuints для хранения указателя, по которому будут храниться данные
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); //Привязываем указатель к названию цели и затем запускаем команду на цель
+                                        //Параметр GL_ARRAY_BUFFER означает, что буфер будет хранить массив вершин
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW); //После связывания нашего объекта, мы наполняем его данными
+    //принимает название цели, размер данных в байтах, адрес массива вершин, и флаг, который обозначает использование паттернов для этих данных.
 }
 
 static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
@@ -146,8 +151,8 @@ static void CompileShaders()
 
 int main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInit(&argc, argv); //Инициализируем GLUT
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); //Настраиваются некоторые опции GLUT. GLUT_DOUBLE включает двойную буферизацию и буфер цвета
     glutInitWindowSize(1024, 768);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Tutorial 07");
@@ -161,13 +166,13 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //Вызов, который устанавливает цвет, который будет использован во время очистки буфера кадра
 
-    CreateVertexBuffer();
+    CreateVertexBuffer(); //создаем вершины 
 
     CompileShaders();
 
-    glutMainLoop();
+    glutMainLoop(); //Этот вызов передаёт контроль GLUT'у, который теперь начнёт свой собственный цикл. будет вызывать только функцию отображения на экран
 
     return 0;
 }
